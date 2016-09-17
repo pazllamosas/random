27334fad7f5e9367d6c276ab20e54497c583b78c
pazllamosas <pazllamosas11@hotmail.com>
2016-09-16 21:30:23 -0300

    script con creacion de esquema

---
+++ data/script_creacion_inicial.sql
@@ -0,0 +1,18 @@
+USE [GD2C2016]
+GO
+
+/****** Object:  Schema [RANDOM]    Script Date: 16/09/2016 21:20:32 ******/
+/**DROP SCHEMA [RANDOM]
+GO**/
+IF  EXISTS (SELECT * FROM sys.schemas WHERE name = N'RANDOM')
+DROP SCHEMA [RANDOM]
+GO
+
+/****** Object:  Schema [RANDOM]    Script Date: 16/09/2016 21:20:32 ******/
+/**CREATE SCHEMA [RANDOM]
+GO**/
+
+CREATE SCHEMA [RANDOM] AUTHORIZATION [dbo]
+GO
+
+
