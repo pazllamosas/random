208e4c2a9df5e0bddefcff177303be64294716d5
ssalvatelli <santixsalva@gmail.com>
2016-10-21 20:06:12 -0300

    Registro de Resultado del Diagnostico

---
+++ src/ClinicaFrba/Registro%20Resultado/Diagnostico.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace ClinicaFrba.Registro_Resultado
+{
+    partial class Diagnostico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTurno = new System.Windows.Forms.Label();
+            this.cckTurno = new System.Windows.Forms.CheckBox();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.lblSintomas = new System.Windows.Forms.Label();
+            this.lblEnfermedad = new System.Windows.Forms.Label();
+            this.txtSintomas = new System.Windows.Forms.TextBox();
+            this.txtEnfermedad = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgvTurnos = new System.Windows.Forms.DataGridView();
+            this.IdTurno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Descripcion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dni = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaYHoraTurno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTurnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTurno
+            // 
+            this.lblTurno.AutoSize = true;
+            this.lblTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurno.Location = new System.Drawing.Point(27, 27);
+            this.lblTurno.Name = "lblTurno";
+            this.lblTurno.Size = new System.Drawing.Size(229, 18);
+            this.lblTurno.TabIndex = 0;
+            this.lblTurno.Text = "Marque si el turno fue concretado";
+            // 
+            // cckTurno
+            // 
+            this.cckTurno.AutoSize = true;
+            this.cckTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cckTurno.Location = new System.Drawing.Point(281, 25);
+            this.cckTurno.Name = "cckTurno";
+            this.cckTurno.Size = new System.Drawing.Size(135, 20);
+            this.cckTurno.TabIndex = 1;
+            this.cckTurno.Text = "Turno Concretado";
+            this.cckTurno.UseVisualStyleBackColor = true;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.Location = new System.Drawing.Point(91, 82);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(119, 18);
+            this.lblFecha.TabIndex = 2;
+            this.lblFecha.Text = "Fecha del Turno:";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Checked = false;
+            this.dtpFecha.Location = new System.Drawing.Point(219, 80);
+            this.dtpFecha.MaxDate = new System.DateTime(3000, 1, 1, 0, 0, 0, 0);
+            this.dtpFecha.MinDate = new System.DateTime(1999, 12, 27, 0, 0, 0, 0);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(270, 20);
+            this.dtpFecha.TabIndex = 3;
+            this.dtpFecha.ValueChanged += new System.EventHandler(this.dtpFecha_ValueChanged);
+            // 
+            // lblSintomas
+            // 
+            this.lblSintomas.AutoSize = true;
+            this.lblSintomas.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSintomas.Location = new System.Drawing.Point(82, 313);
+            this.lblSintomas.Name = "lblSintomas";
+            this.lblSintomas.Size = new System.Drawing.Size(75, 18);
+            this.lblSintomas.TabIndex = 4;
+            this.lblSintomas.Text = "Síntomas:";
+            // 
+            // lblEnfermedad
+            // 
+            this.lblEnfermedad.AutoSize = true;
+            this.lblEnfermedad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnfermedad.Location = new System.Drawing.Point(65, 355);
+            this.lblEnfermedad.Name = "lblEnfermedad";
+            this.lblEnfermedad.Size = new System.Drawing.Size(92, 18);
+            this.lblEnfermedad.TabIndex = 5;
+            this.lblEnfermedad.Text = "Enfermedad:";
+            // 
+            // txtSintomas
+            // 
+            this.txtSintomas.Location = new System.Drawing.Point(181, 313);
+            this.txtSintomas.Name = "txtSintomas";
+            this.txtSintomas.Size = new System.Drawing.Size(375, 20);
+            this.txtSintomas.TabIndex = 6;
+            // 
+            // txtEnfermedad
+            // 
+            this.txtEnfermedad.Location = new System.Drawing.Point(181, 356);
+            this.txtEnfermedad.Name = "txtEnfermedad";
+            this.txtEnfermedad.Size = new System.Drawing.Size(375, 20);
+            this.txtEnfermedad.TabIndex = 7;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnCancelar.Image = global::ClinicaFrba.Properties.Resources.cancel;
+            this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancelar.Location = new System.Drawing.Point(374, 413);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(114, 36);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.Color.Green;
+            this.btnGuardar.Image = global::ClinicaFrba.Properties.Resources.save;
+            this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuardar.Location = new System.Drawing.Point(212, 413);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(114, 36);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgvTurnos
+            // 
+            this.dgvTurnos.AllowUserToAddRows = false;
+            this.dgvTurnos.AllowUserToDeleteRows = false;
+            this.dgvTurnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTurnos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdTurno,
+            this.Nombre,
+            this.Apellido,
+            this.Descripcion,
+            this.Dni,
+            this.FechaYHoraTurno});
+            this.dgvTurnos.Location = new System.Drawing.Point(42, 118);
+            this.dgvTurnos.Name = "dgvTurnos";
+            this.dgvTurnos.ReadOnly = true;
+            this.dgvTurnos.Size = new System.Drawing.Size(584, 150);
+            this.dgvTurnos.TabIndex = 13;
+            // 
+            // IdTurno
+            // 
+            this.IdTurno.HeaderText = "Turno";
+            this.IdTurno.Name = "IdTurno";
+            this.IdTurno.ReadOnly = true;
+            this.IdTurno.Visible = false;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Apellido
+            // 
+            this.Apellido.HeaderText = "Apellido";
+            this.Apellido.Name = "Apellido";
+            this.Apellido.ReadOnly = true;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.HeaderText = "Tipo de Documento";
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.ReadOnly = true;
+            // 
+            // Dni
+            // 
+            this.Dni.HeaderText = "Número";
+            this.Dni.Name = "Dni";
+            this.Dni.ReadOnly = true;
+            // 
+            // FechaYHoraTurno
+            // 
+            this.FechaYHoraTurno.HeaderText = "Fecha y Hora del Turno";
+            this.FechaYHoraTurno.Name = "FechaYHoraTurno";
+            this.FechaYHoraTurno.ReadOnly = true;
+            this.FechaYHoraTurno.Width = 123;
+            // 
+            // Diagnostico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(671, 476);
+            this.Controls.Add(this.dgvTurnos);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtEnfermedad);
+            this.Controls.Add(this.txtSintomas);
+            this.Controls.Add(this.lblEnfermedad);
+            this.Controls.Add(this.lblSintomas);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.cckTurno);
+            this.Controls.Add(this.lblTurno);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Diagnostico";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Diagnóstico";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTurnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTurno;
+        private System.Windows.Forms.CheckBox cckTurno;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label lblSintomas;
+        private System.Windows.Forms.Label lblEnfermedad;
+        private System.Windows.Forms.TextBox txtSintomas;
+        private System.Windows.Forms.TextBox txtEnfermedad;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.DataGridView dgvTurnos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdTurno;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Descripcion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dni;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaYHoraTurno;
+    }
+}
---
+++ src/ClinicaFrba/Registro%20Resultado/Diagnostico.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicaFrba.Registro_Resultado
+{
+    public partial class Diagnostico : Form
+    {
+        public Diagnostico()
+        {
+            InitializeComponent();
+            dtpFecha.Value = DateTime.Today;
+            CargarTurnosDelDia(DateTime.Today);
+        }
+
+        public void CargarTurnosDelDia(DateTime turnoFecha)
+        {
+            dgvTurnos.Rows.Clear();
+            string query = "SELECT T.IdTurno, P.Nombre, P.Apellido, D.Descripcion, P.Dni, T.FechaYHoraTurno ";
+            query = query + "FROM RANDOM.TURNO T, RANDOM.AFILIADO A, RANDOM.PERSONA P, RANDOM.TIPOS_DOCUMENTOS D ";
+            query = query + "WHERE A.NumeroAfiliadoRaiz = T.IdAfiliado AND P.IdPersona = A.IdPersona AND ";
+            query = query + "D.IdTipoDocumento = P.IdTipoDocumento AND DAY(T.FechaYHoraTurno) = " + turnoFecha.Day.ToString();
+            query = query + " AND MONTH(T.FechaYHoraTurno) = " + turnoFecha.Month.ToString() + " AND YEAR(T.FechaYHoraTurno) = ";
+            query = query + turnoFecha.Year.ToString() + " AND T.Habilitado = 0";
+            
+            SqlDataReader reader = Conexion.ejecutarQuery(query);
+
+            while (reader.Read())
+            {
+                dgvTurnos.Rows.Add(reader["IdTurno"], reader["Nombre"], reader["Apellido"], reader["Descripcion"], reader["Dni"], reader["FechaYHoraTurno"]);
+            }
+            reader.Close();
+        }
+
+        private void dtpFecha_ValueChanged(object sender, EventArgs e)
+        {
+            CargarTurnosDelDia(dtpFecha.Value.Date);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Int32 selectedRowCount = dgvTurnos.Rows.GetRowCount(DataGridViewElementStates.Selected);
+            if (selectedRowCount == 1)
+            {
+                if (cckTurno.Checked)
+                {
+                    if (txtSintomas.Text.Length > 0 && txtEnfermedad.Text.Length > 0)
+                    {
+                        DataGridViewRow d = dgvTurnos.SelectedRows[0];
+                        string turno = d.Cells[0].Value.ToString();
+                        string sintomas = txtSintomas.Text;
+                        string enfermedad = txtEnfermedad.Text;
+                        bool resultado = Conexion.executeProcedure("RANDOM.TURNO_CONCRETADO", Conexion.generarArgumentos("@TURNO", "@SINTOMAS", "@ENFERMEDAD"), turno, sintomas, enfermedad);
+                        if (resultado)
+                        {
+                            MessageBox.Show("El diagnóstico finalizó correctamente");
+                        }
+                        this.Hide();
+                        FormProvider.MainMenu.Show();
+                    }
+                    else
+                    {
+                        string mensaje = "";
+                        if (txtSintomas.Text.Length == 0)
+                        {
+                            mensaje += "Debe ingresar un texto en el campo de Síntomas";
+                        }
+                        if (txtEnfermedad.Text.Length == 0)
+                        {
+                            if (txtSintomas.Text.Length == 0)
+                            {
+                                mensaje += " y otro en el de Enfermedad";
+                            }
+                            else
+                            {
+                                mensaje += "Debe ingresar un texto en el campo de Enfermedad";
+                            }
+                        }
+                        MessageBox.Show(mensaje);
+                    }
+                }
+                else
+                {
+                    DataGridViewRow d = dgvTurnos.SelectedRows[0];
+                    string turno = d.Cells[0].Value.ToString();
+                    bool resultado = Conexion.executeProcedure("RANDOM.TURNO_SIN_CONCRETAR", Conexion.generarArgumentos("@TURNO"), turno);
+                    if (resultado)
+                    {
+                        MessageBox.Show("El diagnóstico se finalizó sin concretar");
+                    }
+                    this.Hide();
+                    FormProvider.MainMenu.Show();
+                }
+            }
+            else
+            {
+                if (selectedRowCount == 0)
+                {
+                    MessageBox.Show("Debe seleccionar un turno antes");
+                }
+                else
+                {
+                    MessageBox.Show("Solo podrá procesar un turno a la vez");
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormProvider.MainMenu.Show();
+        }
+    }
+}
