d417d3ee4f0bbb896401ff8f6713e6d714c5a916
melinamazzeo <melinamazzeo93@gmail.com>
2016-11-05 15:35:47 -0300

    cambios

--- src/ClinicaFrba/Registrar Agenda Medico/RegistrarAgenda.Designer.cs
+++ src/ClinicaFrba/Registrar Agenda Medico/RegistrarAgenda.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.btnVolver = new System.Windows.Forms.Button();
-            this.cmbProfesional = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.cmbDias = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -37,11 +36,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.btnBuscar = new System.Windows.Forms.Button();
-            this.btnSeleccionarProfesional = new System.Windows.Forms.Button();
             this.btnSeleccionarTurno = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.cmbProfesional = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnVolver
@@ -58,20 +56,11 @@
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
-            // cmbProfesional
-            // 
-            this.cmbProfesional.FormattingEnabled = true;
-            this.cmbProfesional.Location = new System.Drawing.Point(340, 40);
-            this.cmbProfesional.Name = "cmbProfesional";
-            this.cmbProfesional.Size = new System.Drawing.Size(227, 21);
-            this.cmbProfesional.TabIndex = 35;
-            this.cmbProfesional.SelectedIndexChanged += new System.EventHandler(this.cmbProfesional_SelectedIndexChanged);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(155, 323);
+            this.label1.Location = new System.Drawing.Point(142, 194);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(111, 18);
             this.label1.TabIndex = 37;
@@ -80,9 +69,9 @@
             // cmbDias
             // 
             this.cmbDias.FormattingEnabled = true;
-            this.cmbDias.Location = new System.Drawing.Point(300, 324);
+            this.cmbDias.Location = new System.Drawing.Point(320, 194);
             this.cmbDias.Name = "cmbDias";
-            this.cmbDias.Size = new System.Drawing.Size(227, 21);
+            this.cmbDias.Size = new System.Drawing.Size(206, 21);
             this.cmbDias.TabIndex = 38;
             this.cmbDias.SelectedIndexChanged += new System.EventHandler(this.cmbDias_SelectedIndexChanged);
             // 
@@ -90,18 +79,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(155, 39);
+            this.label2.Location = new System.Drawing.Point(103, 78);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 18);
+            this.label2.Size = new System.Drawing.Size(187, 18);
             this.label2.TabIndex = 39;
-            this.label2.Text = "Seleccione Medico";
+            this.label2.Text = "Ingrese DNI del Profesional";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(44, 368);
+            this.label4.Location = new System.Drawing.Point(85, 274);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 18);
             this.label4.TabIndex = 41;
@@ -111,7 +100,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(400, 368);
+            this.label3.Location = new System.Drawing.Point(380, 274);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(81, 18);
             this.label3.TabIndex = 42;
@@ -120,7 +109,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(158, 369);
+            this.comboBox1.Location = new System.Drawing.Point(191, 275);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(108, 21);
             this.comboBox1.TabIndex = 43;
@@ -129,48 +118,11 @@
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(499, 365);
+            this.comboBox2.Location = new System.Drawing.Point(481, 275);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(108, 21);
             this.comboBox2.TabIndex = 44;
             this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(47, 105);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(571, 139);
-            this.dataGridView1.TabIndex = 45;
-            // 
-            // btnBuscar
-            // 
-            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBuscar.Image = global::ClinicaFrba.Properties.Resources.busqueda;
-            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnBuscar.Location = new System.Drawing.Point(277, 67);
-            this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(96, 32);
-            this.btnBuscar.TabIndex = 46;
-            this.btnBuscar.Text = "Buscar";
-            this.btnBuscar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnBuscar.UseVisualStyleBackColor = true;
-            // 
-            // btnSeleccionarProfesional
-            // 
-            this.btnSeleccionarProfesional.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSeleccionarProfesional.Image = global::ClinicaFrba.Properties.Resources.select;
-            this.btnSeleccionarProfesional.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSeleccionarProfesional.Location = new System.Drawing.Point(173, 260);
-            this.btnSeleccionarProfesional.Name = "btnSeleccionarProfesional";
-            this.btnSeleccionarProfesional.Size = new System.Drawing.Size(308, 44);
-            this.btnSeleccionarProfesional.TabIndex = 47;
-            this.btnSeleccionarProfesional.Text = "Seleccionar Profesional y Especialidad";
-            this.btnSeleccionarProfesional.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.btnSeleccionarProfesional.UseVisualStyleBackColor = true;
             // 
             // btnSeleccionarTurno
             // 
@@ -178,7 +130,7 @@
             this.btnSeleccionarTurno.ForeColor = System.Drawing.Color.Green;
             this.btnSeleccionarTurno.Image = global::ClinicaFrba.Properties.Resources.save;
             this.btnSeleccionarTurno.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSeleccionarTurno.Location = new System.Drawing.Point(238, 416);
+            this.btnSeleccionarTurno.Location = new System.Drawing.Point(246, 365);
             this.btnSeleccionarTurno.Name = "btnSeleccionarTurno";
             this.btnSeleccionarTurno.Size = new System.Drawing.Size(196, 48);
             this.btnSeleccionarTurno.TabIndex = 48;
@@ -186,15 +138,40 @@
             this.btnSeleccionarTurno.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnSeleccionarTurno.UseVisualStyleBackColor = true;
             // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(356, 76);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(151, 20);
+            this.txtDNI.TabIndex = 49;
+            // 
+            // cmbProfesional
+            // 
+            this.cmbProfesional.FormattingEnabled = true;
+            this.cmbProfesional.Location = new System.Drawing.Point(308, 134);
+            this.cmbProfesional.Name = "cmbProfesional";
+            this.cmbProfesional.Size = new System.Drawing.Size(227, 21);
+            this.cmbProfesional.TabIndex = 35;
+            this.cmbProfesional.SelectedIndexChanged += new System.EventHandler(this.cmbProfesional_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(119, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(158, 18);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Selecione especialidad";
+            // 
             // RegistrarAgenda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(671, 476);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDNI);
             this.Controls.Add(this.btnSeleccionarTurno);
-            this.Controls.Add(this.btnSeleccionarProfesional);
-            this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label3);
@@ -210,7 +187,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agenda Profesional";
             this.Load += new System.EventHandler(this.RegistrarAgenda_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,7 +195,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnVolver;
-        public System.Windows.Forms.ComboBox cmbProfesional;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.ComboBox cmbDias;
         private System.Windows.Forms.Label label2;
@@ -227,9 +202,9 @@
         private System.Windows.Forms.Label label3;
         public System.Windows.Forms.ComboBox comboBox1;
         public System.Windows.Forms.ComboBox comboBox2;
-        public System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button btnBuscar;
-        private System.Windows.Forms.Button btnSeleccionarProfesional;
         private System.Windows.Forms.Button btnSeleccionarTurno;
+        private System.Windows.Forms.TextBox txtDNI;
+        public System.Windows.Forms.ComboBox cmbProfesional;
+        private System.Windows.Forms.Label label5;
     }
 }
